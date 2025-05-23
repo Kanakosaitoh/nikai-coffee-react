698753eef9df1cf3385fa53de53de4fd4be74ae7
Kanakosaitoh <kanahanasasha@gmail.com>
2025-05-22 20:29:35 -0700

    frame work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Header />
       </header>
+      <main>
+        <h4 className="brand-concept">Nikai coffee offers Speciality coffee</h4>
+        <img src="/nikai-logo.png" width="300" height="300" />
+      </main>
     </div>
   );
 }
-
-export default App;
